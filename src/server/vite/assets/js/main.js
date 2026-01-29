710d6af85afce74f870d811cde2fcafb6f132a6d
Ozzy <jimeo@mymacewan.ca>
2026-01-28 21:10:37 -0700

    Removed unused cally library

--- src/server/vite/assets/js/main.js
+++ src/server/vite/assets/js/main.js
@@ -1,7 +1,6 @@
 import '../css/main.css';
 import Alpine from 'alpinejs';
 import 'htmx.org/dist/htmx.min.js';
-import "cally";
 import { Observer } from 'tailwindcss-intersect';
 
 window.Alpine = Alpine;

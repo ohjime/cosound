6631893ea005db2e2bac8ddbb9b37088412cb858
Ozzy <jimeo@mymacewan.ca>
2026-04-09 21:46:05 -0600

    Moved Mixer into Root Url since we dont have a HomePage

--- src/server/src/app/urls.py
+++ src/server/src/app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("api/", include("core.urls")),
     path("vote/", include("vote.urls")),
     path("rate/", include("rate.urls")),
-    path("mix/", include("mix.urls")),
+    path("", include("mix.urls")),
 ]

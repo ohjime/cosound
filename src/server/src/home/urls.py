231aaecb5e4157632cdd2686efc2bbd154182d23
Ozzy <jimeo@mymacewan.ca>
2026-04-17 12:41:13 -0600

    New Home mixer

--- src/server/src/home/urls.py
+++ src/server/src/home/urls.py
@@ -9,4 +9,19 @@ urlpatterns = [
     path("swap/", views.swap, name="swap"),
     path("search/", views.search, name="search"),
     path("carousel/", views.carousel, name="carousel"),
+    path(
+        "collect/check_email/",
+        views.collect_check_email,
+        name="collect_check_email",
+    ),
+    path(
+        "collect/check_code/",
+        views.collect_check_code,
+        name="collect_check_code",
+    ),
+    path(
+        "collect/cancel/",
+        views.collect_cancel,
+        name="collect_cancel",
+    ),
 ]
